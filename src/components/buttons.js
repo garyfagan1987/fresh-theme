cf5287e3abb87a34eedd8e21d60b778695d6dd12
Gary Fagan <garyfagan1987@googlemail.com>
2019-12-01 09:30:00 +0000

    adding buttons

---
+++ src/components/buttons.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Buttons = styled.div`
+  display: inline-block;
+
+  button {
+    margin: 0 1rem;
+  }
+
+  button:first-child {
+    margin-left: 0;
+  }
+`;
+
+export default Buttons;
